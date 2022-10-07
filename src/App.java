b30e2a54515e0c0e82fcefa441efce7c2071a05d
🥰 Magda 🥰 <mgd.j@outlook.com>
2022-10-06 22:02:00 -0300

    criação exercicio2

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Informe um número: ");
+
+    int número = sc.nextInt();
+
+    System.out.print("O número digitado foi: ");
+
+    System.out.print(número);
+
+              
+    }
+}
